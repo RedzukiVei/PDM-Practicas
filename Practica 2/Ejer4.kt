7a0b9c60e9f5ea8516516ddd0b0775dde7bf03cf
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 10:34:14 +0000

    Creamos la interfaz y realizamos la sobreesritura de las funciones, se viene una reutilización de código abismal, con tal de dormir.

--- Practica 2/Ejer4.kt
+++ Practica 2/Ejer4.kt
@@ -2,16 +2,36 @@ abstract class Material(val titulo: String, val autor: String, val anoPublicacio
     abstract fun mostrarDetalles()
 }
 
-class Libro(titulo: String, autor: String, anoPublicacion: Int, val genero: String, val numeroPaginas: Int) : Material(titulo, autor, anioPublicacion) {
+class Libro(titulo: String, autor: String, anoPublicacion: Int, val genero: String, val numeroPaginas: Int) : Material(titulo, autor, anoPublicacion) {
     override fun mostrarDetalles() {
         println("Libro: $titulo, Autor: $autor, Año: $anoPublicacion, Género: $genero, Páginas: $numeroPaginas")
     }
 }
 
-class Revista(titulo: String, autor: String, anioPublicacion: Int, val issn: String, val volumen: Int, val numero: Int, val editorial: String) : Material(titulo, autor, anioPublicacion) {
+class Revista(titulo: String, autor: String, anoPublicacion: Int, val issn: String, val volumen: Int, val numero: Int, val editorial: String) : Material(titulo, autor, anoPublicacion) {
     override fun mostrarDetalles() {
-        println("Revista: $titulo, Autor: $autor, Año: $anioPublicacion, ISSN: $issn, Volumen: $volumen, Número: $numero, Editorial: $editorial")
+        println("Revista: $titulo, Autor: $autor, Año: $anoPublicacion, ISSN: $issn, Volumen: $volumen, Número: $numero, Editorial: $editorial")
     }
 }
 
 data class Usuario(val nombre: String, val apellido: String, val edad: Int)
+
+interface IBiblioteca {
+    fun registrarMaterial(material: Material)
+    fun registrarUsuario(usuario: Usuario)
+    fun prestarMaterial(usuario: Usuario, material: Material)
+    fun devolverMaterial(usuario: Usuario, material: Material)
+    fun mostrarMaterialesDisponibles()
+    fun mostrarMaterialesPrestadosPorUsuario(usuario: Usuario)
+}
+
+class Biblioteca : IBiblioteca {
+    private val materialesDisponibles = mutableListOf<Material>()
+    private val usuarios = mutableMapOf<Usuario, MutableList<Material>>()
+
+    override fun registrarMaterial(material: Material) {
+        materialesDisponibles.add(material)
+        println("${material.titulo} registrado.")
+    }
+
+}
