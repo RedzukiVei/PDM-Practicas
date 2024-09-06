10fa5e921d0d4ff79d927b5a77649096531bd8e8
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 10:32:26 +0000

    Agregamos la clase Revista y la clase usuario

--- Practica 2/Ejer4.kt
+++ Practica 2/Ejer4.kt
@@ -1,9 +1,17 @@
-abstract class Material(val titulo: String, val autor: String, val anioPublicacion: Int) {
+abstract class Material(val titulo: String, val autor: String, val anoPublicacion: Int) {
     abstract fun mostrarDetalles()
 }
 
-class Libro(titulo: String, autor: String, anioPublicacion: Int, val genero: String, val numeroPaginas: Int) : Material(titulo, autor, anioPublicacion) {
+class Libro(titulo: String, autor: String, anoPublicacion: Int, val genero: String, val numeroPaginas: Int) : Material(titulo, autor, anioPublicacion) {
     override fun mostrarDetalles() {
-        println("Libro: $titulo, Autor: $autor, Año: $anioPublicacion, Género: $genero, Páginas: $numeroPaginas")
+        println("Libro: $titulo, Autor: $autor, Año: $anoPublicacion, Género: $genero, Páginas: $numeroPaginas")
     }
 }
+
+class Revista(titulo: String, autor: String, anioPublicacion: Int, val issn: String, val volumen: Int, val numero: Int, val editorial: String) : Material(titulo, autor, anioPublicacion) {
+    override fun mostrarDetalles() {
+        println("Revista: $titulo, Autor: $autor, Año: $anioPublicacion, ISSN: $issn, Volumen: $volumen, Número: $numero, Editorial: $editorial")
+    }
+}
+
+data class Usuario(val nombre: String, val apellido: String, val edad: Int)
