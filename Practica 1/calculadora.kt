bab84f7c78fc5e480b7a62fd794c780a0cf68387
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 08:28:19 +0000

    Creamos las funciones básicas, suma, resta, multiplicación y división (creo que le puedo agregar potenciacion y radicacion, pero si lo termino rápido jsjs)

---
+++ Practica 1/calculadora.kt
@@ -0,0 +1,4 @@
+fun suma(a: Double, b: Double) = a + b
+fun resta(a: Double, b: Double) = a - b
+fun multiplicacion(a: Double, b: Double) = a * b
+fun division(a: Double, b: Double) = if (b != 0.0) a / b else Double.NaN
