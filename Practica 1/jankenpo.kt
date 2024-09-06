39f185acece63336ba1d493c412191c4f11b99f4
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 08:09:47 +0000

    Definimos lo que vendrían a ser los mensajes de Empate, derrota y victoria, junto a la comparación de respuestas (sep, no le estoy metiendo el PLUS, pero son las 3 AM)

--- Practica 1/jankenpo.kt
+++ Practica 1/jankenpo.kt
@@ -3,6 +3,15 @@ import kotlin.random.Random
 fun jugarjankenpo(Opcion: String): String {
     val opciones = arrayOf("piedra", "papel", "tijera")
     val seleccionComputadora = opciones[Random.nextInt(3)]
+
+    return when {
+        Opcion == seleccionComputadora -> "Realmente lo hicieron, !EMPATARON!"
+        (Opcion == "piedra" && seleccionComputadora == "tijera") || 
+        (Opcion == "papel" && seleccionComputadora == "piedra") ||
+        (Opcion == "tijera" && seleccionComputadora == "papel") -> "Felicidades, realmente le ganaste al 33%, sientete orgulloso"
+        
+        else -> "Lastima bro, pero recuerda, hasta los mejores pierden"
+     }
 }
 
 fun main (){
