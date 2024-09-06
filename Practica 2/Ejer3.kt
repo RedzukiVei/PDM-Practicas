4f40a1844229179abeeefdf250f8149caa927a51
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 10:24:59 +0000

    Reutilizando la copia que tenia en el intellij y salvando todo el proceso, pero dios, ya quiero terminar ;-;

--- Practica 2/Ejer3.kt
+++ Practica 2/Ejer3.kt
@@ -1,42 +1,36 @@
 abstract class Shape {
-    abstract fun CalcularArea(): Double
-    abstract fun CalcularPerimetro(): Double
+    abstract fun calcularArea(): Double
+    abstract fun calcularPerimetro(): Double
 
-    fun ImprimirResultados(){
-        println("La Área es: ${CalcularArea()}")
-        println("El Perimetro es: ${CalcularPerimetro()}")
+    fun imprimirResultados() {
+        println("Área: ${calcularArea()}")
+        println("Perímetro: ${calcularPerimetro()}")
     }
 }
 
 class Cuadrado(private val lado: Double) : Shape() {
-
     override fun calcularArea(): Double = lado * lado
-
     override fun calcularPerimetro(): Double = 4 * lado
 }
 
 class Circulo(private val radio: Double) : Shape() {
-
-    override fun calcularArea(): Double = Math.PI * radio * radio           //Se utilzia Math.PI para llamar al poderoso PI osea 3.14
-
+    override fun calcularArea(): Double = Math.PI * radio * radio               //Se utilzia Math.PI para llamar al poderoso PI osea 3.14
     override fun calcularPerimetro(): Double = 2 * Math.PI * radio
 }
 
 class Rectangulo(private val largo: Double, private val ancho: Double) : Shape() {
-
     override fun calcularArea(): Double = largo * ancho
-
     override fun calcularPerimetro(): Double = 2 * (largo + ancho)
 }
 
 fun main() {
-    var continuar = true
+    val cuadrado = Cuadrado(4.0)
+    
+    val circulo = Circulo(3.0)
+    
+    val rectangulo = Rectangulo(5.0, 2.0)
 
-    while (continuar) {
-        println("Elige una figura:")
-        println("1. Cuadrado")
-        println("2. Círculo")
-        println("3. Rectángulo")
-        println("4. Salir")
-    }
+    cuadrado.imprimirResultados()
+    circulo.imprimirResultados()
+    rectangulo.imprimirResultados()
 }
