19e47bf79705eb3d2bf7410f66a045a55ef8a449
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 10:28:11 +0000

    Creo la clase Libro que tendrá la info acerca del libro y la clase abstracta material, que tendrá la info del autor, publicacion y titulo.

---
+++ Practica 2/Ejer4.kt
@@ -0,0 +1,9 @@
+abstract class Material(val titulo: String, val autor: String, val anioPublicacion: Int) {
+    abstract fun mostrarDetalles()
+}
+
+class Libro(titulo: String, autor: String, anioPublicacion: Int, val genero: String, val numeroPaginas: Int) : Material(titulo, autor, anioPublicacion) {
+    override fun mostrarDetalles() {
+        println("Libro: $titulo, Autor: $autor, Año: $anioPublicacion, Género: $genero, Páginas: $numeroPaginas")
+    }
+}
