b959c02829e10f0a397c02e2dda425c29b36143e
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 08:05:23 +0000

    creamos las opciones y la eleccion de la computadora que se eligira de forma aleatoria entre las 3 opciones creadas.

--- Practica 1/jankenpo.kt
+++ Practica 1/jankenpo.kt
@@ -1,9 +1,10 @@
 import kotlin.random.Random
 
 fun jugarjankenpo(Opcion: String): String {
-
+    val opciones = arrayOf("piedra", "papel", "tijera")
+    val seleccionComputadora = opciones[Random.nextInt(3)]
 }
 
 fun main (){
-    
+
 }
