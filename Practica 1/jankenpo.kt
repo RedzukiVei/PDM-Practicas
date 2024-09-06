ccd527a7f68f41183836f69f00a880f27f23872f
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 08:53:01 +0000

    Agregando "Detalles".

--- Practica 1/jankenpo.kt
+++ Practica 1/jankenpo.kt
@@ -1,10 +1,10 @@
 import kotlin.random.Random
 
 fun jugarjankenpo(Opcion: String): String {
-    val opciones = arrayOf("piedra", "papel", "tijera")
-    val seleccionComputadora = opciones[Random.nextInt(3)]
+    val opciones = arrayOf("piedra", "papel", "tijera")             //Creamos las opciones de siempre en jankenpo
+    val seleccionComputadora = opciones[Random.nextInt(3)]          //Hacemos un randomizador para la eleccion del CPU
 
-    return when {
+    return when {                                                   //Realizamos las respuestas de empate, victoria y derrota
         Opcion == seleccionComputadora -> "Realmente lo hicieron, !EMPATARON!"
         (Opcion == "piedra" && seleccionComputadora == "tijera") || 
         (Opcion == "papel" && seleccionComputadora == "piedra") ||
@@ -15,16 +15,16 @@ fun jugarjankenpo(Opcion: String): String {
 }
 
 fun main (){
-    println("Elige pibe:")
+    println("Elige pibe:")                                          //Menú principal de eleccion
     println("piedra")
     println("papel")
     println("tijera")
     
-    val Opcion = readLine()
+    val Opcion = readLine()                                         //Lee la opcion que elegimos
     
-    val resultado = Opcion?.let{ jugarjankenpo(it) }
+    val resultado = Opcion?.let{ jugarjankenpo(it) }                //Evitamos que regrese un null mediante Opcion?.let y leemos jugarjankenpo
 
-    if (resultado != null) {
+    if (resultado != null) {                                        //Un if en caso de que no encuentre una opción válida
         println(resultado)
     } else {
         println("No se proporcionó una opción válida")
