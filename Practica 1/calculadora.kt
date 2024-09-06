7f254f862ae743e39b4e71aa495ab58fab3107a6
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 08:32:19 +0000

    Creo el menú de lo que será nuestra calculadora.

--- Practica 1/calculadora.kt
+++ Practica 1/calculadora.kt
@@ -2,3 +2,16 @@ fun suma(a: Double, b: Double) = a + b
 fun resta(a: Double, b: Double) = a - b
 fun multiplicacion(a: Double, b: Double) = a * b
 fun division(a: Double, b: Double) = if (b != 0.0) a / b else Double.NaN
+
+fun main() {
+    var opcion: Int
+    do {
+        println("==== Menú ====")
+        println("1. Suma")
+        println("2. Resta")
+        println("3. Multiplicacion")
+        println("4. Division")
+        println("Elige pibe, que operación matemática deseas realizar (solo elige el número): ")
+        opcion = readLine()!!.toInt()
+    }
+}
