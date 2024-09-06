3da0803d22c0ecc32040c31316b7345bee1d6c9b
RedzukiVei <142834904+RedzukiVei@users.noreply.github.com>
2024-09-06 09:44:51 +0000

    Creando la funcion retirar y asignandole, las posibilidades que puede haber a la hora de retirar.

--- Practica 2/CuentaBancaria.kt
+++ Practica 2/CuentaBancaria.kt
@@ -10,4 +10,15 @@ class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double
             println("El saldo no puede estar en números rojos, mi estimado)
         }
     }
+
+    fun retirar(cantidad: Double) {              //Creamos la funcion retira
+        if (cantidad > limiteRetiro) {          //if en caso de que nos pasemos del limite
+            println("Usted esta excediendo la cantidad de retiro que tiene.")
+        } else if (cantidad > saldo) {           //if en caso de que la cantidad en retirar sea superior al saldo
+            println("No cuenta con el saldo suficiente.")
+        } else {
+            saldo -= cantidad                   //Si se cumple el retiro, se restara al saldo, la cantidad retirada
+            println("Has logrado retirar con exito. Su saldo actual es: $$saldo")
+        }
     }
+}
